660cd2004c0b0f614f5000bf0305923e7fff9a98
hpamula <hubertspamula@gmail.com>
2025-05-15 04:41:12 +0200

    added first version of setup

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+sudo apt update && sudo apt upgrade -y
+sudo apt install nodejs npm
+# sudo apt install sqlite3 libsqlite3-dev
+npx express-generator backend --no-view
+cd backend
+# who knows, maybe command below is redundant
+# npm install
+npm install sqlite3
+# npm start
+cd ..
+npm create vite@latest frontend -- --template react
+cd frontend
+# who knows, maybe command below is redundant
+# npm install
+# after `npm start` in one terminal, run below in another terminal to run both
+# npm run dev
