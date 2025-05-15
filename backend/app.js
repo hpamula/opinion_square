f3bb9692687439659985e4a7d6b3a2ab4f2c7f5d
hpamula <hubertspamula@gmail.com>
2025-05-16 00:56:48 +0200

    added to backend very refined app.js and bin/www

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+// Disable caching FIRST (for development only)
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
+});
+app.use(require('morgan')('dev'));
+// app.use(express.json()); // for REST API
+// app.use(express.urlencoded({ extended: false })); // for classic HTML forms
+// app.use(express.static(require('path').join(__dirname, 'public')));
+app.use('/', require('./routes/index'));
+// app.use('/users', require('./routes/users'));
+
+module.exports = app;
