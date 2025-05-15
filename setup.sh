ed57025dec7e4dc30228875295a8a87c28615fda
hpamula <hubertspamula@gmail.com>
2025-05-15 05:36:06 +0200

    added 3rd version of setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# ==================== GLOBAL INSTALLS ====================
 set -e
 sudo apt update && sudo apt upgrade -y
 sudo apt install nodejs npm
 # sudo apt install sqlite3 libsqlite3-dev
+# ==================== LOCAL BACKEND SETUP ====================
 npx express-generator backend --no-view
 cd backend
 # who knows, maybe command below is redundant
 # npm install
+# ===================== LOCAL BACKEND DATABASE SETUP ====================
+# Execution flow: routes -> controllers -> services -> models -> config (database)
+# Everything except routes can be omitted
+# Here stays: routes (with sql querries) -> models with config (database definitions)
 npm install sqlite3
 # npm start
-# maybe mkdir models db controllers
+mkdir database
+# ==================== LOCAL FRONTEND SETUP ====================
 cd ..
 npm create vite@latest frontend -- --template react
 cd frontend
