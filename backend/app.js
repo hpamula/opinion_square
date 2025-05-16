ff2b6d9c49c6cbd958d3028e63d93248096c83a4
hpamula <hubertspamula@gmail.com>
2025-05-16 03:37:42 +0200

    changed backend structure, no automatic template anymore

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+path = require('path');
 const app = express();
 // Disable caching FIRST (for development only)
 app.use((req, res, next) => {
@@ -8,9 +9,13 @@ app.use((req, res, next) => {
 app.use(require('morgan')('dev'));
 // app.use(express.json()); // for REST API
 // app.use(express.urlencoded({ extended: false })); // for classic HTML forms
-// app.use(express.static(require('path').join(__dirname, 'public')));
-app.use('/', require('./routes/index'));
-// app.use('/users', require('./routes/users'));
+
+app.get('/', (req, res) => {
+  res.send('hello from the backend');
+});
 app.use((req, res) => {res.status(404).send(`<h1>Page does not exist</h1>`)});
 
-module.exports = app;
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
--- setup.sh
+++ setup.sh
@@ -7,23 +7,17 @@ sudo apt install nodejs npm
 # sudo apt install sqlite3 libsqlite3-dev
 
 # ==================== LOCAL BACKEND SETUP ====================
-npx express-generator backend --no-view
-cd backend
-# who knows, maybe command below is redundant
-# npm install
-
-# ===================== LOCAL BACKEND DATABASE SETUP ====================
 # Execution flow: routes -> controllers -> services -> models -> config (database)
-# Everything except routes can be omitted
-# Here stays: routes (with sql querries) -> models with config (database definitions)
-npm install sqlite3
-# npm start
-mkdir database
+mkdir backend && cd backend
+npm install express sqlite3 morgan
+# mkdir database
+node app.js
 
 # ==================== LOCAL FRONTEND SETUP ====================
-cd ..
-npm create vite@latest frontend -- --template react
-cd frontend
+# commented beacuse not finished yet
+# cd ..
+# npm create vite@latest frontend -- --template react
+# cd frontend
 # who knows, maybe command below is redundant
 # npm install
 # after `npm start` in one terminal, run below in another terminal to run both
